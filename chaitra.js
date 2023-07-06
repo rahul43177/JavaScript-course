39784c01661ed4eacd63f65d4b71a0ebe2ac1c8d
Rahul <rahulmishra4317@gmial.com>
2023-07-06 20:55:21 +0530

    done

--- chaitra.js
+++ chaitra.js
@@ -23,22 +23,98 @@
 // console.log(array[0]) //here index is key 
 //i want to create a profile of a girl where it should have -> fname , lname , age , gender , job , phone number
 
-var profile = {
-    fname : "Chaitra" ,
-    lname : "Mishra" ,
-    age : 88 , 
-    gender : "female" ,
-    job : "Sonata" ,
-    phone : 9611814477
+// var profile = {
+//     fname : "Chaitra" ,
+//     lname : "Mishra" ,
+//     age : 88 , 
+//     gender : "female" ,
+//     job : "Sonata" ,
+//     phone : 9611814477
+// }
+
+// //change it to my profile 
+// profile.fname  = "Rahul"
+// profile.lname  = "Kabbur"
+// profile.age = 5
+// profile.gender = "male"
+// profile.job = "FUp" 
+// profile.phone = 8747905930
+// //add a new key -> location 
+// profile["location"] = "Bangalore"
+// console.log(profile)
+
+// var a = 12 //day 1 
+// function1 //
+// function2 
+// function3
+
+
+
+
+// //day 60 
+// var a =  23 
+// // let a = 12 
+
+// // let a = 23 
+// // console.log(a)
+
+//if we know that the value of the variable is going to change in the future 
+//let 
+//const -> value will be same in the entire program 
+
+
+// let a = 12 
+// console.log(a)
+// a = 23 
+// console.log(a)
+
+// let a = "Rahul"
+
+// const a = "Rahul"
+
+// a = "Chaitra"
+// console.log(a)
+// let a = 12 
+// console.log(a--) //12 -> //11
+// let array = [200,500,600,700,800]
+// let slice = array.slice(2)
+// console.log(slice)
+
+// % => remainder
+
+// let a = 12 
+
+//to check whether a is even or not 
+// if(a%2 == 0) {
+//     console.log("Even")
+// }
+// else {
+//     console.log("Odd")
+// }
+// const number = 10;
+// const result = number % 2 == 0 ? 'Even' : 'Odd';
+// console.log(result);
+
+//write a ternary operator to check whether the number is odd or not 
+// let number = 14 
+// let checkOdd = number % 2 != 0 ? "Odd" : "Even" 
+// console.log(checkOdd)
+
+// to whether the number is 1 or  print yes , otherwise no 
+let number = 1 
+if(number ==1 ) {
+    console.log("yes")
+}
+else if(number == 0) { //else if 
+    console.log("Yes")
+}
+else {
+    console.log("No")
 }
 
-//change it to my profile 
-profile.fname  = "Rahul"
-profile.lname  = "Kabbur"
-profile.age = 5
-profile.gender = "male"
-profile.job = "FUp" 
-profile.phone = 8747905930
-//add a new key -> location 
-profile["location"] = "Bangalore"
-console.log(profile)
+if(number == 1 || number == 0) {
+    console.log("Yes")
+}
+else {
+    console.log("No")
+}
